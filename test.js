795791922abdc17aa6f309c780ea634aa22f6565
ehdcjf <kdch0823@naver.com>
2021-09-06 02:10:10 +0900

    백준 입력폼

--- test.js
+++ test.js
@@ -1,3 +1,23 @@
 const fs = require('fs');
-const input = fs.readFileSync("./dev/stdin.js").toString().trim();
+const input = fs.readFileSync("./dev/stdin").toString().trim().split("\r\n");
+
+
+const [n, max] = input[0].trim().split(' ').map(v => +v);
+const card = input[1].trim().split(' ').map(v => +v);
+
+let answer = 0;
+
+for (let i = 0; i < n; i++) {
+  for (let j = i + 1; j < n; j++) {
+    for (let k = j + 1; k < n; k++) {
+      let temp = card[i] + card[j] + card[k];
+      if (temp <= max) {
+        if (temp > answer)
+          answer = temp;
+      }
+    }
+  }
+}
+console.log(answer);
+
 
